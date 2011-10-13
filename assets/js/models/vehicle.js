62008888c99d79871fa9c79dee71a4bd4d534346
Jesper Josefsson <jesper.josefsson@gmail.com>
2011-10-13 22:27:13 +0200

    Added vehicle model

---
+++ assets/js/models/vehicle.js
@@ -0,0 +1,33 @@
+App.models.Vehicle = Backbone.Model.extend({
+  /* Constructor
+  * arg: shape - Raphael element to be animated
+  */
+  initialize: function(shape) {
+    this.set({shape: shape});
+    this.get("shape").hover(function() {
+      this.glow({width: 10});
+    });
+  },
+  startTowards: function(x,y) {
+    this.set({dest_x: x});
+    this.set({dest_y: y});
+    var animation = Raphael.animation({cx: x, cy: y}, 50000);
+    this.set({animation: animation});
+    this.get("shape").animate(animation);
+    return this;
+  },
+  start: function() {
+    this.get("shape").stop().animate(this.get("animation"));
+    return this;
+  },
+  setNewTime: function(time) {
+    this.get("animation").ms = time;
+    return this.start();
+  },
+  moveTo: function(x, y) {
+    return this.get("shape").stop().attr({cx: x, cy: y});
+  },
+  remove: function() {
+    return this.get("shape").hide();
+  }
+});
