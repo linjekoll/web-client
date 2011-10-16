083a297a4e535a2bb683479c27e945ab5d937a7c
Linus Oleander <linus@oleander.nu>
2011-10-16 11:44:57 +0200

    Include order should be predifined for js files

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,4 +1,8 @@
-//= require_tree ./lib
+//= require ./lib/jquery
+//= require ./lib/underscore
+//= require ./lib/backbone
+//= require ./lib/raphael
+
 //= require_tree ./controllers
 //= require_tree ./models
 //= require_tree ./views
