c3c2ee69f1e509ed6250eb63acc38941d4a7ef0d
Linus Oleander <linus@oleander.nu>
2011-10-19 18:05:47 +0200

    Moving the 'just for fun' code to it's own file

---
+++ assets/javascripts/lib/funfunfun.js
@@ -0,0 +1,18 @@
+window.v = function() {
+  window.vm = new App.models.Vehicle();
+  window.vv = new App.views.Vehicle({model: window.vm, shape: lineView.paper.circle(10,10,10)});
+};
+
+window.vstart = function() {
+  window.vm.setTrip({time: 10000, destination: {x: 900, y:10}});
+};
+
+window.vnewtime = function (time) {
+  window.vm.set({time: time});
+};
+
+window.stop = function () {
+  window.stop2 = new App.models.Stop({x: 100, y:100, name: "Mölndal"});
+  window.stop2View = new App.views.Stop({model: stop2, paper: window.lineView.paper});
+  window.stop2View.render();
+}
